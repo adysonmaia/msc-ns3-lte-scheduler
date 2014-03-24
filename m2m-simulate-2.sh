91d7b8e8dc8f74126fdf10a6c9ea76afadc70bf5
Adyson Magalhães Maia <adyson.maia@gmail.com>
2014-03-24 14:32:17 +0100

    atualizacao no script

--- m2m-simulate-2.sh
+++ m2m-simulate-2.sh
@@ -21,15 +21,18 @@ nRbM2M=3
 minPerRbM2M=0.48
 
 for index in {0..9}
+#for index in {10..19}
+#for index in {20..29}
 do
     paramsGeneral="--simTime=$simTime --nH2HVoIP=$nH2HVoIP --nH2HVideo=$nH2HVideo --nH2HFTP=$nH2HFTP --nM2MTrigger=$nM2MT --nM2MRegular=$nM2MR --intervalM2MTrigger=$intTrigger --minM2MRegularCqi=$minCqi --maxM2MRegularCqi=$maxCqi --minRBPerH2H=$nRbH2H --nExec=$index"
-    for delayWeight in 0.7 0.5 1 0 0.8 0.3 0.9
+    for delayWeight in 1 0.8 0.75 0.72 0.7 0.5 0.3 0
     do
         paramsGeneral="$paramsGeneral --ns3::M2mMacScheduler::M2MDelayWeight=$delayWeight"
         for nRbM2M in 3
 #        for nRbM2M in {3..5}
         do
-            for nMinM2M in 2 3 4
+#            for nMinM2M in 2 3 4
+            for nMinM2M in 4
             do
                 minPerRbM2M=`python -c "print $nRbM2M*$nMinM2M/$bandwidth.0"`
                 fileSuffix="$delayWeight-$nRbM2M-$minPerRbM2M"
